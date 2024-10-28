f73dca91897689f199105e9d2c0ef688686f6804
Steve Park <dimplejuno@gmail.com>
2024-10-28 02:40:24 +0000

    fix it

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+plt.plot([1, 2, 3, 4], 'o-r')
+plt.ylabel('some numbers')
+plt.show()
